af4c4282539c700c05e4370d207b6284644d27b7
azadprajapat <azadprajapat4@gmail.com>
2021-06-07 12:52:37 +0530

    heroku publish2

---
+++ config/main.js
@@ -0,0 +1,4 @@
+module.exports={
+    PORT:process.env.PORT||3000,
+    DATABASE:"mongodb+srv://akash20:akash20*@makomdeivery.oizas.mongodb.net/test"
+}
---
+++ config/secrets.js
@@ -0,0 +1,7 @@
+// 64 bit random bytes
+module.exports = {
+    AUTHSECRET: 'gPsaMNQQC3ScUvPWelkHOlVpmPZtHTMmD1wtnnViFmcJ3Mq-1VpK9xDj8aCBSB_U',
+    ADMINSECRET: 'ko2T31J65SmmkyYMnauJrU3lSyZ9S4XhTW9n6lyPUV5nhbhDTk0HIEb4A9J4y3gU',
+    OTPSECRET: 'WfjRLaRvtzvEN5W9inNmg8pEHTTan74pu8IaZ5ztuRv1KWd9_PYgIYePvlN3ZKHY',
+    MANAGERSECRET: 'QbZnstPknrlNmIwcJBqYzgWYd6SvpTHmGIyZgzH4whIKZNm_uaPkoYlj69ApjVOx',
+}
