fedc71649c4a2401b5623bb7bd984e1db3c427b2
azadprajapat <azadprajapat4@gmail.com>
2021-06-25 17:59:20 +0530

    updated database

--- config/main.js
+++ config/main.js
@@ -1,6 +1,5 @@
 module.exports={
     PORT:process.env.PORT||8080,
-   //DATABASE:"mongodb+srv://akash20:akash20*@makomdeivery.oizas.mongodb.net/test"
-   DATABASE:"mongodb://127.0.0.1:27017/makom"
-
-}
+   // DATABASE:"mongodb://127.0.0.1:27017/makom"
+   DATABASE:"mongodb://makom:##Makom##@127.0.0.1:27017/makom"
+ }
